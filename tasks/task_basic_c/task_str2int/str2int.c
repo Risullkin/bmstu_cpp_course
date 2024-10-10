b34c86624f8cd7597584dbc28e4341b896093cb5
artem <shuranov16@bk.ru>
2024-10-10 11:32:14 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,43 +1,37 @@
-#include <assert.h>
+#include <stdlib.h> 
+#include <stdio.h> 
+#include "stdbool.h" 
+#include <assert.h> 
 #include "str2int.h"
-#include <printf.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "stdbool.h"
-
-
-int str2int(const char *str) {
-    if (*str == '-') {
-        assert(1 == 0);
-    }
-    return 0;
-}
-
-bool space(char c) {
-    return (c == ' ');
-}
-bool digit(char c) {    
-    return (c >= '0' & c <= '9');
-}
-int otvet(const char *str) {
-    int result = 0;
+ 
+bool space(char c) { 
+    return (c == ' '); 
+} 
+bool digit(char c) {     
+    return (c >= '0' & c <= '9'); 
+} 
+int str2int(const char *str) { 
+    int result = 0; 
     int sign = 1;
-    while (space(*str)) {
-        str++;    
+    bool flag = 1;
+    while (space(*str)) { 
+        str++;     
+    } 
+    if (*str == '-' || *str == '+') { 
+        sign = (*str == '-') ? -1 : 1;         
+        str++; 
+    } 
+    while (digit(*str)) {       
+        int digit = *str - 0x30;  
+        if (((sign == -1) && (result > (-2147483647 - 1 - digit) / 10)) || ((sign == 1) && (result > (2147483647 - digit) / 10))) { 
+            assert(0==1); 
+        } 
+        result = result * 10 + digit;  
+        flag = 0;      
+        str++; 
+    } 
+    if (flag) {
+        assert(0==1);
     }
-    if (*str == '-' || *str == '+') {
-        sign = (*str == '-') ? -1 : 1;        
-        str++;
-    }
-    while (digit(*str)) {      
-        int digit = *str - 0x30;
-        result = result * 10 + digit;        
-        str++;
-    }
-
-    return result * sign;
-}
-int main(void){
-    assert(otvet("    10") == 10);
-    return 0;
+    return result * sign; 
 }
