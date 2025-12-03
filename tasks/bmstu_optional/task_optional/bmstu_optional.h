a26fa2858fbff3b7aa610110a4c26b8aa72da65c
artem <shuranov16@bk.ru>
2025-12-04 01:06:45 +0300

    Шуранов сделал optional

--- tasks/bmstu_optional/task_optional/bmstu_optional.h
+++ tasks/bmstu_optional/task_optional/bmstu_optional.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 #include <exception>
 #include <type_traits>
+#include <utility>
 
 namespace bmstu
 {
@@ -16,7 +18,7 @@ class bad_optional_access : public std::exception
    public:
 	using exception::exception;
 
-	const char* what() const noexcept override { return "WRONG TEXT"; }
+	const char* what() const noexcept override { return "Bad optional access"; }
 };
 
 template <typename T>
@@ -25,66 +27,181 @@ class optional
    public:
 	optional() = default;
 
-	optional(const T& value) {}
+	optional(const T& value) : is_initialized_(true) { new (data_) T(value); }
 
-	optional(T&& value) {}
+	optional(T&& value) : is_initialized_(true)
+	{
+		new (data_) T(std::move(value));
+	}
 
-	optional(const optional& other) {}
+	optional(const optional& other) : is_initialized_(other.is_initialized_)
+	{
+		if (is_initialized_)
+		{
+			new (data_) T(*other);
+		}
+	}
 
-	optional(optional&& other) noexcept {}
+	optional(optional&& other) noexcept : is_initialized_(other.is_initialized_)
+	{
+		if (is_initialized_)
+		{
+			new (data_) T(std::move(*other));
+			other.is_initialized_ = false;
+		}
+	}
 
-	optional& operator=(const T& value) {}
+	optional& operator=(const T& value)
+	{
+		if (is_initialized_)
+		{
+			**this = value;
+		}
+		else
+		{
+			new (data_) T(value);
+			is_initialized_ = true;
+		}
+		return *this;
+	}
 
-	optional& operator=(T&& value) {}
+	optional& operator=(T&& value)
+	{
+		if (is_initialized_)
+		{
+			**this = std::move(value);
+		}
+		else
+		{
+			new (data_) T(std::move(value));
+			is_initialized_ = true;
+		}
+		return *this;
+	}
 
-	optional& operator=(const optional& value) {}
+	optional& operator=(const optional& value)
+	{
+		if (this != &value)
+		{
+			if (value.is_initialized_)
+			{
+				if (is_initialized_)
+				{
+					**this = *value;
+				}
+				else
+				{
+					new (data_) T(*value);
+					is_initialized_ = true;
+				}
+			}
+			else
+			{
+				reset();
+			}
+		}
+		return *this;
+	}
 
-	optional& operator=(optional&& value) {}
+	optional& operator=(optional&& value)
+	{
+		if (this != &value)
+		{
+			if (value.is_initialized_)
+			{
+				if (is_initialized_)
+				{
+					**this = std::move(*value);
+				}
+				else
+				{
+					new (data_) T(std::move(*value));
+					is_initialized_ = true;
+				}
+				value.is_initialized_ = false;
+			}
+			else
+			{
+				reset();
+			}
+		}
+		return *this;
+	}
 
 	T& operator*() &
 	{
-		T* ptr = nullptr;
+		if (!is_initialized_)
+		{
+			throw bad_optional_access{};
+		}
+		T* ptr = reinterpret_cast<T*>(data_);
 		return *ptr;
 	}
 
 	const T& operator*() const&
 	{
-		T* ptr = nullptr;
+		if (!is_initialized_)
+		{
+			throw bad_optional_access{};
+		}
+		const T* ptr = reinterpret_cast<const T*>(data_);
 		return *ptr;
 	}
 
-	T* operator->() { return nullptr; }
+	T* operator->() { return &(**this); }
 
-	const T* operator->() const { return nullptr; }
+	const T* operator->() const { return &(**this); }
 
 	T&& operator*() &&
 	{
-		T* ptr = nullptr;
+		if (!is_initialized_)
+		{
+			throw bad_optional_access{};
+		}
+		T* ptr = reinterpret_cast<T*>(data_);
 		return std::move(*ptr);
 	}
 
 	T& value() &
 	{
-		T* ptr = nullptr;
+		if (!is_initialized_)
+		{
+			throw bad_optional_access{};
+		}
+		T* ptr = reinterpret_cast<T*>(data_);
 		return *ptr;
 	}
 
 	const T& value() const&
 	{
-		T* ptr = nullptr;
+		if (!is_initialized_)
+		{
+			throw bad_optional_access{};
+		}
+		const T* ptr = reinterpret_cast<const T*>(data_);
 		return *ptr;
 	}
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
+		reset();
+		new (data_) T(std::forward<Args>(args)...);
+		is_initialized_ = true;
 	}
 
-	void reset() {}
+	void reset()
+	{
+		if (is_initialized_)
+		{
+			reinterpret_cast<T*>(data_)->~T();
+			is_initialized_ = false;
+		}
+	}
 
-	~optional() {}
+	~optional() { reset(); }
 
-	bool has_value() const { return false; };
+	bool has_value() const { return is_initialized_; };
 
    private:
 	alignas(T) uint8_t data_[sizeof(T)];
